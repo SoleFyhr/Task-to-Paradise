82e05b307be7bed8e6c6eaa386e71138a5ebeba8
SoleFyhr <rousseaunico@free.fr>
2024-03-12 23:31:41 +0100

    tried to fix ios bug

--- tasks-to-paradise/src/core/App.js
+++ tasks-to-paradise/src/core/App.js
@@ -21,34 +21,17 @@ import Settings from "../script/Settings";
 import Login from "../script/Login";
 import backgroundImage from "../imgs/dark.jpg"; // Adjust the path according to your project structure
 
-
 const isIOS = () => {
   return (
     /iPad|iPhone|iPod/.test(navigator.userAgent) ||
-    (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)
+    (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1)
   );
-}
+};
 
 const isIOSDevice = isIOS(); // Call the utility function
-const apiUrl = process.env.API_URL || '';
+const apiUrl = process.env.API_URL || "";
 
-const iosStyle = css`
-  #root::before {
-    content: '';
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    z-index: -1;
-    background-image: url(${backgroundImage});
-    background-color: var(--second-bg-color);
-    background-blend-mode: overlay;
-    background-repeat: no-repeat;
-    background-size: cover;
-  }
-`;
-const defaultStyle = css`
+const globalStyle = css`
   html,
   body {
     margin: 0;
@@ -57,20 +40,31 @@ const defaultStyle = css`
     color: white;
   }
 
-  #root {
-    min-height: 100%;
-    display: flex;
-    flex-direction: column;
+  #background-container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
     background-image: url(${backgroundImage});
-    background-color: var(--second-bg-color);
+    background-color: var(
+      --second-bg-color
+    ); 
     background-blend-mode: overlay;
-    background-attachment: fixed !important;
     background-repeat: no-repeat;
-    background-size: cover !important;
+    background-size: cover;
+  }
+
+  #root {
+    min-height: 100vh; /* Ensure content can scroll */
+    position: relative;
+    z-index: 1; /* Ensures content is above the background */
   }
 `;
+
+
 function App() {
-  
   const [loggedInUser, setLoggedInUser] = useState(null);
   const { setColorMode } = useColorMode();
 
@@ -85,19 +79,6 @@ function App() {
       setLoggedInUser(storedUser);
     }
   }, []);
-
-  useEffect(() => {
-    if (isIOSDevice) {
-      const handleScroll = () => {
-        const offset = window.pageYOffset;
-        document.getElementById('root').style.setProperty('--bg-position', `0px ${offset}px`);
-      };
-
-      window.addEventListener('scroll', handleScroll);
-
-      return () => window.removeEventListener('scroll', handleScroll);
-    }
-  }, [isIOSDevice]);
 
   const handleLogin = (username) => {
     setLoggedInUser(username);
@@ -128,7 +109,8 @@ function App() {
 
   return (
     <>
-      <Global styles={isIOSDevice ? iosStyle : defaultStyle} />
+      <Global styles={globalStyle} />
+      <div id="background-container"></div>
 
       <header>
         <Router>
@@ -224,7 +206,7 @@ export default App;
 //?S'occuper du scaling des weekly et company
 //?enlever les rewards daily?. Que weekly and monthly. //Si penality weekly alors pas de reward weekly. pour le monthly, mettre ses objectifs en début de mois. (avoir une alerte qui tous les jours pop et dit de le faire)
 
-//Code project 
+//Code project
 //Test project
 
 //Fonction extend de tache qui consomme la moitié des points d'importance le dernier jour, et qui prolonge d'un jour mais en faisant x1.5 les points d'importance. Edit possible d'une tache jusqu'à 2 jours avant la complétion de la tache.
@@ -233,8 +215,7 @@ export default App;
 //Rendre la penalty induced d'une task hoverable dans le dashboard avec un popover avec le content de la penalty induced et avec genre une étoile rouge en mode asterix.
 //Rendre la valeur des ppoints d'une task customizable, et si c'est le cas, dans app.js lors de la création de task regarder si on peut int() la valeur
 
-
-//faire que le pause mode empeche tout 
+//faire que le pause mode empeche tout
 //make an anti "I tap multiple times and i get 3* the planified rewards because it was sent 3 times by the js"
 //Mettre les popover dans settings
 //Rajouter dans le modal quand on veut un setting (soit complétion, soit importance etc), le nom des catégories (good, average...) en fonction de ce qu'on a décidé de changer (faire des listes contenants le nom des variables dans le modal, comme dans le modal pour task) et mettre l'ancienne valeur directement dans le modal comme j'ai pu faire dans le edit de penalties etc
@@ -248,8 +229,6 @@ export default App;
 //Prohibited : The deduction could be a fixed amount or vary based on the severity or frequency of the violation.
 //mot de passe  - Add authentification with google or smth not crackable
 
-
-
 //Comprehensive Scoring System: Develop a scoring system that integrates completion levels, task importance, and prohibitions. This system should be transparent and easily understandable to the users.
 //Dynamic Thresholds for Rewards/Penalties: Adjust weekly and monthly reward/penalty thresholds based on average task difficulty, completion levels, and prohibitions. This makes the system more adaptive and personalized.
 
@@ -259,13 +238,7 @@ export default App;
 //Consecutive Achievements: If users hit their targets consecutively (like achieving the weekly reward several weeks in a row), provide a bonus multiplier to reward consistency.
 //Special Bonuses: Introduce surprise bonuses for extraordinary achievements, like completing all tasks in a month or hitting the highest threshold multiple times in a row.
 
-
-
-
 //ajouter une petite alerte/message ephémère pour dire quand c'est checké que c'est bien pris en compte.
 //Rendre le responsive plus beau ? (settings...)
 //rendre plus joli tasks penlatys et rewards
 //Faire un beau dark mode.
-
-
-
